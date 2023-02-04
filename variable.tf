74e6f81b4168a526d3770438a583854f429ebd50
root <root@ip-172-31-15-186.ap-south-1.compute.internal>
2023-02-04 05:23:21 +0000

    final

---
+++ variable.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  type =string
+  default = "ap-south-1"
+  
+}
+
+variable "subnet_id_1" {
+
+  type = string
+
+  default = "subnet-01b5527b562d5976a"
+
+ }
+ variable "subnet_id_2" {
+
+  type = string
+
+  default = "subnet-0ae2eebeda6b2329e"
+
+ }
+ variable "subnet_id_3" {
+  
+  type = string
+
+  default = "subnet-07416b7bf78f7b659"
+
+ }
+
+ variable "vpc_id" {
+  type = string
+  default = "	vpc-009ab9b8be2e3f180"
+   
+ }
